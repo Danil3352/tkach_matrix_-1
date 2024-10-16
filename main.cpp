f171f20681e2ffd74cbee4ac50dd4d75f5ced941
Danil3352 <danil.tkach2018@yandex.ru>
2024-10-16 13:06:33 +0300

    Main upgrade 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-
+int** ct(size_t m, size_t n);
+void dt(int **t, size_t m, size_t n);
 int main(){
   size_t M=0,N=0;
   std::cin >> M >> N;
+  int **t = ct(M,N);
+  dt(t,M,N);
   std::cout << M << " " << N << "\n";
 }
